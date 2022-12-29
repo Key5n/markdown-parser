db8c9e1b38497d1e567e141407254eafec7bfb3f
key5n <k.nogami.472@stn.nitech.ac.jp>
2022-12-30 00:08:35 +0900

    deleted arg size from tokenize

--- markdown/parser.c
+++ markdown/parser.c
@@ -6,15 +6,13 @@ const char paragraph[] = "p";
 text_elm_regexp_t h1_regexps = {.elm_type = H1, .regexp = "^# (.+)$"};
 
 /**
- * @brief listにタグ名をelm_name, 中の文章をresult[1]としたトークンを作る
+ * @brief listにタグ名をelm_name, 中の文章をcontentとしたトークンを作る
  * @param list ArrayList<Token_t> この中に入れる
  * @param content トークン化したときの中身の文章
  * @param elm_name トークンのタグ名 (e.g. h1, h2, p)
- * @param size result配列のサイズ
  * 正規表現の実行で確保したresult配列のメモリを解放するのに使う
  */
-static List_T __tokenize(List_T list, char *content, const char *elm_name,
-                         size_t size);
+static List_T __tokenize(List_T list, char *content, const char *elm_name);
 
 List_T __parse(char *original_text, List_T asts) {
   // 今回は正規表現にマッチするグループは2つだけ
@@ -28,7 +26,7 @@ List_T __parse(char *original_text, List_T asts) {
   // original textを正規表現の篩にかけた結果マッチするものがあるなら
   if ((size = regexp(t.regexp, original_text, result)) != FAILURE) {
     // h1タグとして変換
-    asts = __tokenize(asts, result[1], t.elm_type, size);
+    asts = __tokenize(asts, result[1], t.elm_type);
     // for (i = 0; i < size; i++) {
     //   free(result[i]);
     // }
@@ -36,16 +34,15 @@ List_T __parse(char *original_text, List_T asts) {
   } else {
     // 今回はh1タグに変換できないならpタグとして変換
 
-    asts = __tokenize(asts, original_text, paragraph, size);
+    asts = __tokenize(asts, original_text, paragraph);
     // for (i = 0; i < size; i++) {
-    //   free(result[i]);
+    // free(result[i]);
     // }
     return asts;
   }
 }
 
-static List_T __tokenize(List_T asts, char *content, const char *elm_name,
-                         size_t size) {
+static List_T __tokenize(List_T asts, char *content, const char *elm_name) {
   size_t i = 0;
   List_T list = (List_T)malloc(sizeof(Token_t));
   Token_t *token = (Token_t *)malloc(sizeof(Token_t));
