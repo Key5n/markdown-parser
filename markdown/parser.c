94db6d88e88c11fc8e8be7577a91b9c80520f794
key5n <k.nogami.472@stn.nitech.ac.jp>
2022-12-30 01:46:07 +0900

    removed comment

--- markdown/parser.c
+++ markdown/parser.c
@@ -12,13 +12,12 @@ text_elm_regexp_t h1_regexps = {.elm_type = H1, .regexp = "^# (.+)$"};
  * @param elm_name トークンのタグ名 (e.g. h1, h2, p)
  * 正規表現の実行で確保したresult配列のメモリを解放するのに使う
  */
-static List_T __tokenize(List_T list, char *content, const char *elm_name);
+static List_T tokenize(List_T list, char *content, const char *elm_name);
 
-List_T __parse(char *original_text, List_T asts) {
-  // 今回は正規表現にマッチするグループは2つだけ
+List_T parse(char *original_text, List_T asts) {
+  // 今回は正規表現にマッチするグループは全体一致と(.+)の2つのグループだけ
   const size_t match_size = 2;
-  // char *result[match_size];
-  char **result = (char **)malloc(sizeof(char) * match_size);
+  char **result = (char **)malloc(sizeof(char *) * match_size);
   size_t size = 0;
   size_t i = 0;
   text_elm_regexp_t t = h1_regexps;
@@ -26,26 +25,20 @@ List_T __parse(char *original_text, List_T asts) {
   // original textを正規表現の篩にかけた結果マッチするものがあるなら
   if ((size = regexp(t.regexp, original_text, result)) != FAILURE) {
     // h1タグとして変換
-    asts = __tokenize(asts, result[1], t.elm_type);
-    // for (i = 0; i < size; i++) {
-    //   free(result[i]);
-    // }
+    asts = tokenize(asts, result[1], t.elm_type);
     return asts;
   } else {
     // 今回はh1タグに変換できないならpタグとして変換
 
-    asts = __tokenize(asts, original_text, paragraph);
-    // for (i = 0; i < size; i++) {
-    // free(result[i]);
-    // }
+    asts = tokenize(asts, original_text, paragraph);
     return asts;
   }
 }
 
-static List_T __tokenize(List_T asts, char *content, const char *elm_name) {
-  size_t i = 0;
+static List_T tokenize(List_T asts, char *content, const char *elm_name) {
   List_T list = (List_T)malloc(sizeof(Token_t));
-  Token_t *token = (Token_t *)malloc(sizeof(Token_t));
+  // List_T list;
+  Token_t *token = malloc(sizeof(Token_t));
 
   // 文字列の領域を確保
   // 中身がSTRING_BUFを超えたらどうしよう
